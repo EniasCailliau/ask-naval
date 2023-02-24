a674ef4a29a772d38102d200e4e2d4b38577c0a7
Enias Cailliau <enias@steamship.com>
2023-02-24 15:40:50 +0100

    Remove npm stane in navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -64,7 +64,7 @@ export default function Nav({ path, deployButton }: NavProps) {
                 More Examples →
               </Button>
             </span>
-npm          </nav>
+          </nav>
         </div>
       </div>
     </nav>
